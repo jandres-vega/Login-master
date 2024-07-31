140b2d8c24854b94c75d2dd394b81cd042d88eb4
jandres-vega <jandresvega@itsa.edu.co>
2024-07-31 11:35:08 -0500

    feat: Add registerUserController for user registration

---
+++ api-login/src/controllers/auth/post/auth.controller.ts
@@ -0,0 +1,17 @@
+import {NextFunction, Request, Response} from "express";
+import {User} from "../../../types";
+import {AuthService} from "../../../services";
+
+const authService = new AuthService();
+
+const registerUserController = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const user:User = req.body;
+        const newUser = await authService.registerUser(user);
+        res.status(200).json({message: newUser});
+    }catch (e){
+        next(e);
+    }
+}
+
+export {registerUserController};
