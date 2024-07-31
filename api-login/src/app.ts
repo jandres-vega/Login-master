33406f33e51ee786bae3784b6e099a08ab4a7bba
jandres-vega <jandresvega@itsa.edu.co>
2024-07-31 11:36:02 -0500

    chore: Update app.ts with middleware and router setup

--- api-login/src/app.ts
+++ api-login/src/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import {corsOptions} from "./config";
-
+import {router} from "./routes";
+import {boomErrorHandler, errorHandler, logErrors} from "./middlewares";
 
 const app = express();
 app.use(cors(corsOptions));
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+router(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 export {app};
