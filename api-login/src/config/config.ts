68358f963fa837e6a44a3ef62415635c9b33d2ef
jandres-vega <jandresvega@itsa.edu.co>
2024-07-31 13:23:34 -0500

    Update jwt_secret to cast it as a string in config.ts

--- api-login/src/config/config.ts
+++ api-login/src/config/config.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const config= {
     port_server: process.env.PORT,
     database_url: process.env.DATABASE_URL,
-    jwt_secret: process.env.JWT_SECRET,
+    jwt_secret: process.env.JWT_SECRET as string,
     email_host: process.env.EMAIL_HOST,
     bcrypt_salt: process.env.BCRYPT_SALT,
 };
