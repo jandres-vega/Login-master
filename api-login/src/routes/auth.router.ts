006747d82d7e045912a5d3c0637a1b544ee08050
jandres-vega <jandresvega@itsa.edu.co>
2024-07-31 11:35:25 -0500

    feat: Add auth router for user registration

---
+++ api-login/src/routes/auth.router.ts
@@ -0,0 +1,8 @@
+import {Router} from "express";
+import {registerUserController} from "../controllers";
+
+const router = Router();
+
+router.post('/register', registerUserController);
+
+export default router;
