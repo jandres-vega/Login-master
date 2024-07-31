04772a64bbd0074b0c59ff87317864808dd968ec
jandres-vega <jandresvega@itsa.edu.co>
2024-07-31 13:24:36 -0500

    feat: Add user authentication functionality

--- api-login/src/services/auth/auth.service.ts
+++ api-login/src/services/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import {User} from "../../types";
+import {PayloadLogin, User} from "../../types";
 import * as bcrypt from 'bcrypt';
 import {UserService} from "../user/user.service";
 import {prisma} from "../../libs";
 import Boom from "@hapi/boom";
+import jwt from 'jsonwebtoken';
+import {config} from "../../config";
 
 const userService = new UserService();
 
@@ -27,6 +29,29 @@ class AuthService {
         });
         if (!newUser) throw Boom.badRequest('Error al crear el usuario');
         return 'Usuario creado exitosamente';
+    }
+
+    async getUser(email:string, password:string){
+        const userFind:User = await userService.getUserByEmail(email) as User;
+        if (!userFind){
+            throw Boom.unauthorized('No estas autorizado');
+        }
+        const isMatch = await bcrypt.compare(password, userFind.password);
+        if (!isMatch){
+            throw Boom.unauthorized('No estas autorizado');
+        }
+        return {
+            email: userFind.email,
+            name: userFind.name
+        };
+    }
+
+    async signIn(user:User){
+        const payload:PayloadLogin = {
+            sub: user.id,
+            role: user.role
+        };
+        return jwt.sign(payload, config.jwt_secret, {expiresIn: '1d'});
     }
 }
 
